0dbbcc873e7cddfb04ab8ab1a6d34516e3f89519
Rudra Yadav <realrudrayadav@gmail.com>
2024-08-20 01:52:34 +0530

    course selling app

--- test0.js
+++ test0.js
@@ -4,10 +4,25 @@ const jwt =require("jsonwebtoken");
 
 const secret ="superS3cr3t1";
 
+
+//encrypting
 let ans = jwt.sign("abc" , secret);
 console.log(ans);
-
+//decrypting
 jwt.verify(ans , secret , (err , originalString)=>{
     console.log("asadadsadmk");
     console.log(originalString);
 })
+
+//we can encrypt and decrypt the object
+let ans2 =jwt.sign({
+    username: "Rudra@123",
+    password: "123456"
+} , secret);
+console.log(ans);
+
+jwt.verify(ans , secret , (err , originalstring2)=>{
+        console.log("afhsikuf");
+        console.log(originalstring2);
+})
+ 
